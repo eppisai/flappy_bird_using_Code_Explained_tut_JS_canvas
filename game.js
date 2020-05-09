5e02e5a44aa95ba03eb212784774d2dfb4a531ac
thealpha <theamanamanaman@gmail.com>
2020-05-09 20:27:17 +0530

    added comments and enhanced the code for better reading

--- game.js
+++ game.js
@@ -1,18 +1,28 @@
 // JAVASCRIPT CODE //
+
+// Canvas elements
 const cvs = document.getElementById("Bird");
 const ctx = cvs.getContext("2d");
+
+// Game Variables
 let frames = 0;
 
+
+// draw function
 function draw() {
     ctx.fillStyle = "#70c5ce";
     ctx.fillRect(0, 0, cvs.width, cvs.height);
 
 }
 
+
+// update function
 function update() {
 
 }
 
+
+//loop function
 function loop() {
     update();
     draw();
