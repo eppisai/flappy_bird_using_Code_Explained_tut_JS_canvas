83af3dec7c329fc2bb063f3951230f73d8a9dd2f
thealpha <theamanamanaman@gmail.com>
2020-05-09 20:47:35 +0530

    added the city and the road in game.js

--- game.js
+++ game.js
@@ -7,11 +7,45 @@ const ctx = cvs.getContext("2d");
 // Game Variables
 let frames = 0;
 
+// loading images
+const sprite = new Image();
+sprite.src = "img/sprite.png";
 
+
+// the background
+const bg = {
+    sX: 0,
+    sY: 0,
+    w: 275,
+    h: 226,
+    x: 0,
+    y: cvs.height - 226,
+    draw: function () {
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+    }
+
+}
+
+// the forground
+const fg = {
+    sX: 276,
+    sY: 0,
+    w: 224,
+    h: 112,
+    x: 0,
+    y: cvs.height - 112,
+    draw: function () {
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+    }
+}
 // draw function
 function draw() {
     ctx.fillStyle = "#70c5ce";
     ctx.fillRect(0, 0, cvs.width, cvs.height);
+    bg.draw();
+    fg.draw();
 
 }
 
