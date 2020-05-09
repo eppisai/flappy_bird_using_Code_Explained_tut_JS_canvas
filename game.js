62caa1bcd0277a3b54172dcc8aa33a843c414ea7
thealpha <theamanamanaman@gmail.com>
2020-05-09 20:08:07 +0530

    created loop function, that calls draw(), and update() and then requests animation frames
    cvs =  query selector for canvas
    ctx =  object with properties and methods that you can use to render graphics inside the canvas element

--- game.js
+++ game.js
@@ -1,1 +1,17 @@
 // JAVASCRIPT CODE //
+const cvs = document.getElementById("Bird");
+const ctx = cvs.getContext("2d");
+let frames = 0;
+
+function draw() {
+    ctx.fillStyle = "#70c5ce";
+    ctx.fillRect(0, 0, cvs.clientWidth, cvs.height);
+
+}
+
+function loop() {
+    update();
+    draw();
+    frames++;
+    requestAnimationFrame(loop);
+}
